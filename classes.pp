927232e1f58ec74722acd118c813197a9304ed96
Dick Davies <rasputnik@hellooperator.net>
2009-05-10 20:58:09 +0100

    Add a mySQL class, turn off NTPd on these VMs, make sure services are enabled as well as running, and reformat.

--- classes.pp
+++ classes.pp
@@ -1,6 +1,9 @@
 # classes common to all servers
 class baseclass {
-  include mailsetup, sshsetup, ntpsetup
+  # VirtualBox clocks drift so far when laptop sleeps
+  # that NTPd can't fix it, so disable in that case
+  ## include ntpsetup
+  include mailsetup, sshsetup
 
   #give fair warning
   file { "/etc/motd": content => "This box is managed by Puppet.\n" }
@@ -11,10 +14,10 @@ class mailsetup {
   exec { "newaliases": refreshonly => true }
 
   mailalias { "rootalias":
-    ensure => present,
+    ensure    => present,
     recipient => 'rasputnik@hellooperator.net',
-    name => 'root',
-    notify => Exec['newaliases']
+    name      => 'root',
+    notify    => Exec['newaliases']
   }
 }
 
@@ -24,11 +27,14 @@ class sshsetup {
   $permitrootlogin = 'without-password'
 
   file { "/etc/ssh/sshd_config" :
-    notify => Service['sshd'],
+    notify  => Service['sshd'],
     content => template("sshd_config.erb"),
   }
 
-  service { "sshd" : ensure => running }
+  service { "sshd" :
+    ensure => running,
+    enable => true
+  }
   package { "openssh-server" : ensure => present }
 
 # this goes into a separate file for safety
@@ -42,11 +48,34 @@ class ntpsetup {
     $ntpserver = '10.0.0.254'
 
     file { "ntp.conf" :
-      name => '/etc/ntp.conf',
+      name    => '/etc/ntp.conf',
       content => template('ntp.conf.erb'),
-      notify => Service['ntpd']
+      notify  => Service['ntpd']
     }
 
-    service { "ntpd" : ensure => running }
     package { "ntp" : ensure => present }
+    service { "ntpd" :
+      ensure => running,
+      enable => true
+    }
+}
+
+# setup mysql-server
+class mysqldb {
+
+  package { 'mysql-server' : ensure => present }
+  service { "mysqld" :
+    ensure => running,
+    enable => true
+  }
+ 
+  # changing this here will *not* magically change it on the server 
+  $mysqlrootpass = 'sekrti'
+  # designed to only run when the package is installed
+  exec { 'Set mysql root password' :
+    command     => "mysqladmin -uroot password $mysqlrootpass",
+    unless      => "mysqladmin -uroot -p$mysqlrootpass status",
+    subscribe   => Package['mysql-server'],
+    refreshonly => true
+  }
 }
--- nodes.pp
+++ nodes.pp
@@ -1,4 +1,6 @@
 # centOS 5.3 boxes
 node 'node01' { include baseclass }
 node 'node02' { include baseclass }
-node 'node03' { include baseclass }
+node 'node03' {
+  include baseclass, mysqldb
+}

d275bcbf7e36633478eda56888b7241117dc0544
Dick Davies <rasputnik@hellooperator.net>
2009-05-21 21:10:54 +0100

    describe how to syntax check

--- site.pp
+++ site.pp
@@ -1,5 +1,10 @@
 import "classes"
 import "nodes"
 
+# you can syntax check this file with
+# 'puppet --parseonly site.pp'
+# it won't catch more than the most basic errors,
+# but is better than nothing
+
 # global default PATH for all exec resources
 Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin" }

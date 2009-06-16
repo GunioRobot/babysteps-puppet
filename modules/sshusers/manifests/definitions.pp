cce798d862766e189adfebafcfc0245b61d4a361
Dick Davies <rasputnik@hellooperator.net>
2009-06-16 22:22:20 +0100

    split definition into its own file

---
+++ modules/sshusers/manifests/definitions.pp
@@ -0,0 +1,43 @@
+# simplify creating a local account
+# with no password, but an RSA/DSA key
+#
+# (see bottom of file for how this definition is used)
+
+define ssh_user($comment,$uid,$group,$mail) {
+
+  # create the user
+  user { "$name":
+    home       => "/home/$name",
+    managehome => true,
+    comment    => $comment,
+    gid        => $group,
+    require    => Group[$group],
+    ensure     => present,
+    password   => '!',
+    uid        => $uid,
+    shell      => '/bin/bash'
+  }
+
+  # create ~/.ssh (~/ is created by User[$name])
+  file { "/home/$name/.ssh":
+    ensure  => directory,
+    mode    => 700,
+    owner   => $name,
+    group   => $group,
+    require => User[$name]
+  }
+  file { "/home/$name/.ssh/authorized_keys":
+    source  => "puppet:///sshusers/authorized_keys/$name",
+    mode    => 400,
+    owner   => $name,
+    group   => $group,
+    require => File["/home/$name/.ssh"]
+  }
+
+  mailalias { "$name":
+    ensure    => present,
+    recipient => $mail,
+    notify    => Exec['newaliases']
+  }
+
+}
--- modules/sshusers/manifests/init.pp
+++ modules/sshusers/manifests/init.pp
@@ -1,48 +1,5 @@
-# simplify creating a local account
-# with no password, but an RSA/DSA key
-#
-# (see bottom of file for how this definition is used)
+import 'definitions.pp'
 
-define ssh_user($comment,$uid,$group,$mail) {
-
-  # create the user
-  user { "$name":
-    home       => "/home/$name",
-    managehome => true,
-    comment    => $comment,
-    gid        => $group,
-    require    => Group[$group],
-    ensure     => present,
-    password   => '!',
-    uid        => $uid,
-    shell      => '/bin/bash'
-  }
-
-  # create ~/.ssh (~/ is created by User[$name])
-  file { "/home/$name/.ssh":
-    ensure  => directory,
-    mode    => 700,
-    owner   => $name,
-    group   => $group,
-    require => User[$name]
-  }
-  file { "/home/$name/.ssh/authorized_keys":
-    source  => "puppet:///sshusers/authorized_keys/$name",
-    mode    => 400,
-    owner   => $name,
-    group   => $group,
-    require => File["/home/$name/.ssh"]
-  }
-
-  mailalias { "$name":
-    ensure    => present,
-    recipient => $mail,
-    notify    => Exec['newaliases']
-  }
-
-}
-
-# now actually use it
 class sshusers {
 
   group { "sysadmins": gid => 5000 }
